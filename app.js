516498d0acbe728438d8ff37baaa8321c3f8d1bc
ekspil <ekspil@yandex.ru>
2020-08-04 10:46:45 +1000

    new

---
+++ app.js
@@ -0,0 +1,345 @@
+﻿var express = require('express');
+const fetch = require('node-fetch')
+var app = express();
+var http = require('http');
+var iconv = require('iconv-lite');
+var restorants = [];
+restorants[0] = "192.168.15.95:3006";
+restorants[1] = "192.168.15.166:4000";
+restorants[2] = "10.12.0.23:3000";
+restorants[3] = "192.168.15.95:3007";
+restorants[4] = "10.13.0.20:3000";
+restorants[5] = "10.3.0.20:3000";
+restorants[6] = "192.168.15.95:3006";
+restorants[7] = "10.4.0.20:3000";
+restorants[8] = "10.15.0.13:3000";
+restorants[9] = "10.18.0.20:3000";
+restorants[10] = "10.6.0.4:4000";
+restorants[11] = "10.2.0.25:3000";
+restorants[12] = "10.8.0.20:3000";
+restorants[13] = "10.9.0.16:3000";
+restorants[14] = "10.11.0.16:3000";
+restorants[15] = "10.14.0.19:3000";
+restorants[16] = "192.168.15.95:3006";
+var sendStation = [];
+sendStation[0] = 0;
+sendStation[1] = 1;
+sendStation[2] = 2;
+sendStation[3] = 3;
+sendStation[4] = 4;
+sendStation[5] = 5;
+sendStation[6] = 5;
+sendStation[7] = 7;
+var port = 3003;
+http.Server(app).listen(port);
+const https = require('https');
+
+var secret = 'asd123zxc';
+var order = {};
+app.get('/test', function(req,res){
+var urla = req.query.url;
+var num = 0;
+intervalFunc(urla, num);
+res.status(200).end();
+
+
+});
+
+
+function selectStation(data){
+
+if(data == 0 || data == 1 || data == 2 || data == 3 || data == 4 || data == 5 || data == 6 || data == 7 || data == 8 || data == 10 || data == 11 ||  data == 9 ||  data == 12 ||  data == 13 || data == 14 || data == undefined){
+if(data == 1 || data == 6 || data == 3){
+  return 1;
+}
+else if(data == undefined){
+  return 0;
+}
+else if (data == 2 || data == 5) {
+  return 2;
+}
+else if (data == 11) {
+  return 4;
+}
+}
+else {
+
+  if(data == 'сбор'){
+    return 1;
+  }
+  else if(data == undefined){
+    return 0;
+  }
+  else if (data == 'фри') {
+    return 2;
+  }
+  else if (data == 'пиц') {
+    return 4;
+  }
+  return 0;
+}
+
+
+}
+
+function intervalFunc(urla, num) {
+  num++;
+  if(num > 10){
+    return;
+  }
+  http.get('http://'+urla, (ress) => {
+  const { statusCode } = ress;
+  let error;
+  if (statusCode !== 200) {
+    error = new Error('Ну удалось отправить сообщение на '+urla+' !');
+  }
+  if (error) {
+    console.error(error.message);
+    ress.resume();
+    return;
+  }
+  //console.log("Сообщение на "+urla+" успешно  доставлено, попытка завершена!")
+  return;
+
+}).on('error', (e) => {
+ console.error('Ну удалось отправить сообщение на '+urla+' !');
+ let thisdate = new Date();
+ console.log(thisdate);
+ console.log(e);
+  });
+}
+
+
+function sendData(name, unit, id, num, rest, station) {
+  num++;
+  if(num > 10){
+    return;
+  }
+  name = encodeURI(name);
+  http.get('http://'+restorants[rest]+'/new/?name='+name+'&unit='+unit+'&id='+id+'&station='+station, (ress) => {
+  const { statusCode } = ress;
+  let error;
+  if (statusCode !== 200) {
+    error = new Error('Ну удалось отправить сообщение позиции '+id+' !');
+  }
+  if (error) {
+    let thisdate = new Date();
+    console.log(thisdate);
+    console.error(error.message);
+    ress.resume();
+    return;
+  }
+  return;
+
+}).on('error', (e) => {
+ console.error('Ну удалось отправить сообщение позиции '+id+' !');
+
+  });
+}
+
+
+function sendDataPaid(id, typeCheck, num, rest, codeApp, client_name) {
+  if(client_name){
+      client_name = encodeURI(client_name)
+  }else{
+      client_name = ''
+  }
+
+  num++;
+  if(codeApp){
+    codeApp = String(codeApp)
+  }else{
+    codeApp = ""
+  }
+  if(num > 10){
+    return;
+  }
+
+  http.get('http://'+restorants[rest]+'/newCheck/?id='+id+'&checkType='+typeCheck+'&code='+codeApp+'&guestName='+client_name+'', (ress) => {
+    let thisdate = new Date();
+  console.log("\r \n Отправлен заказ: " + id + " , ip: " + restorants[rest] + " , дата: "+thisdate);
+  const { statusCode } = ress;
+  let error;
+  if (statusCode !== 200) {
+    error = new Error('Ну удалось отправить сообщение чека '+id+'  !');
+  }
+  if (error) {
+    console.error(error.message);
+    ress.resume();
+    return;
+  }
+  return;
+}).on('error', (e) => {
+ console.error('Ну удалось отправить сообщение '+id+' !');
+  });
+}
+
+
+app.get('/updateorder', async function(req,res){
+
+  let token = req.query.token;
+  if(token == '234Gwdli2g') {
+  try {
+    console.log(1)
+    let orderid = req.query.order_id;
+    const result = await fetch('https://delivery.rb24.ru/common_api/order/'+orderid+'?apikey=ZmFkMTlhNzQyMGRhMGI4N2NlOTQwZTI0MmQ3OTk1MTU3NjIwMmRkMA', {
+      method: "GET"
+    })
+    console.log(2)
+    const json = await result.json()
+    await fetch(`http://${restorants[json.workshop_id]}/take_out/?order=${orderid}&comment=${encodeURI(json.comment)}`, {
+      method: "GET"
+    })
+    console.log(4)
+    res.sendStatus(200)
+  }
+  catch (e) {
+    res.sendStatus(503)
+  }
+
+  }
+  else{
+    res.sendStatus(503)
+  }
+})
+
+app.get('/getorder', function(req,res){
+var token = req.query.token;
+if(token == '234Gwdli2g'){
+
+var orderid = req.query.order_id;
+var restid = req.query.rest;
+
+order[orderid] = {};
+
+https.get('https://delivery.rb24.ru/common_api/order/'+orderid+'?apikey=ZmFkMTlhNzQyMGRhMGI4N2NlOTQwZTI0MmQ3OTk1MTU3NjIwMmRkMA', (ress) => {
+  const { statusCode } = ress;
+  const contentType = ress.headers['content-type'];
+
+  let error;
+  if (statusCode !== 200) {
+    error = new Error('Request Failed.\n' +
+                      `Status Code: ${statusCode}`);
+  } else if (!/^application\/json/.test(contentType)) {
+    error = new Error('Invalid content-type.\n' +
+                      `Expected application/json but received ${contentType}`);
+  }
+  if (error) {
+    console.error(error.message);
+    ress.resume();
+    return;
+  }
+
+
+  let rawData = [];
+  var newhtml = '';
+  ress.on('data', (chunk) => { rawData.push(chunk); });
+  ress.on('end', () => {
+    try {
+      var decodedBody = iconv.decode(Buffer.concat(rawData), 'utf8');
+      const parsedData = JSON.parse(decodedBody);
+      if(!parsedData.client_name){
+          parsedData.client_name = ''
+      }
+      //console.log(parsedData);
+      var type = parsedData.source;
+      let typeNum = 3
+      if (type == "site" || type == "phone"){
+        typeNum = 3;
+      }
+      else if(type == "mobile_app"){
+
+        if(parsedData.is_pickup_app && parsedData.pickup_takeaway){
+            typeNum = 4;
+        }
+        if(parsedData.is_pickup_app && !parsedData.pickup_takeaway){
+            typeNum = 5;
+        }
+
+      }
+      else{
+        typeNum = 3;
+      }
+
+        if (parsedData.workshop_id){
+        let debug = {
+            id: parsedData.id,
+            source: parsedData.source,
+            workshop_id: parsedData.workshop_id,
+            isApp: parsedData.is_pickup_app,
+            codeApp: parsedData.code
+
+
+        };
+        //console.log(debug);
+      for(var i = 0; i < parsedData.positions.length; i++){
+
+
+        for( var ii = 0; ii < parsedData.positions[i].quantity; ii++){
+          var item_id = orderid+"P"+i+"C"+ii;
+
+          var ifBillInfo = parsedData.positions[i].bill_info;
+
+          if(!ifBillInfo){
+            sendData(parsedData.positions[i].name, orderid, item_id, 0, parsedData.workshop_id, selectStation(parsedData.positions[i].main_section_id[0]));
+
+          }
+          else{
+            var bill_info_arr = parsedData.positions[i].bill_info.split('/');
+            for (var iii = 0; iii < bill_info_arr.length; iii++){
+              if(bill_info_arr[iii]){
+                var bill_info_positions = bill_info_arr[iii].split(',');
+
+                for (var iiii = 0; iiii < bill_info_positions[1]; iiii++){
+                  var item_id_set = orderid+"P"+i+"C"+ii+"S"+iii+"N"+iiii;
+                    sendData(bill_info_positions[0], orderid, item_id_set, 0, parsedData.workshop_id, selectStation(bill_info_positions[2].replace(' ', '')));
+
+                }
+              }
+            }
+          }
+
+        }
+
+      }
+
+      setTimeout(sendDataPaid, 2000, orderid, typeNum, 0, parsedData.workshop_id, parsedData.code, parsedData.client_name);
+
+      var jsonData = {};
+        jsonData.error = "0";
+        jsonData = JSON.stringify(jsonData);
+        jsonData = JSON.parse(jsonData);
+
+      res.status(200).json(jsonData);
+
+    }
+    else{
+      var jsonData = {};
+        jsonData.error = "1";
+        jsonData = JSON.stringify(jsonData);
+        jsonData = JSON.parse(jsonData);
+      console.log("Не указан магазин");
+      res.status(200).json(jsonData);
+    }
+
+
+    } catch (e) {
+      console.error(e.message);
+    }
+
+  });
+}).on('error', (e) => {
+  console.error(`Got error: ${e.message}`);
+});
+
+
+
+
+
+}
+else{
+
+  res.status(401).end();
+}
+
+});
---
+++ mssql.js
@@ -0,0 +1,501 @@
+﻿var Connection = require('tedious').Connection;
+var Request = require('tedious').Request;
+var TYPES = require('tedious').TYPES;
+var async = require('async');
+var http = require('http');
+
+// Create connection to database
+var config = {
+  userName: 'sa', // update me
+  password: 'Cfvjktn1', // update me
+  server: '192.168.15.150',
+  options: {
+    database: 'Cards',
+    rowCollectionOnRequestCompletion: true
+  }
+}
+
+var connection = new Connection(config);
+
+function getRandomInt(min, max)
+{
+return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function readInfo(callback, id, res) {
+    console.log('Reading Phone Info...');
+    id = "'"+id+"'";
+
+
+
+
+
+    // Read all rows from table
+    request = new Request(
+    'SELECT id, summa, fname, date, phone_number, pass FROM cards.dbo.list WHERE phone_number = '+id+';',
+    function(err, rowCount, rows) {
+    if (err) {
+      var result = {};
+      result.phone_number = "error SQL";
+        callback(err, result, res);
+    } else {
+        console.log(rowCount + ' row(s) returned');
+        if (rowCount == 0){
+          var result = {};
+          result.phone_number = "notRegistred";
+          callback(null, result, res);
+        }
+        else if (rowCount == 1){
+          var result = {};
+          var row = rows[0];
+          result.id = row[0].value;
+          result.summa = row[1].value;
+          result.fname = row[2].value;
+          result.date = row[3].value;
+          result.phone_number = row[4].value;
+          result.pass = row[5].value;
+          callback(null, result, res);
+
+
+      }
+      else if (rowCount > 1){
+        var result = {};
+        result.phone_number = "manyRegistred";
+        callback(null, result, res);
+      }
+
+    }
+    });
+
+    // Print the rows read
+
+
+
+    // Execute SQL statement
+    connection.execSql(request);
+}
+
+
+
+function readInfoCard(callback, id, res) {
+
+    if (id > 999999999 ){
+      id = 01;
+    }
+    console.log('Reading Phone Info...' + id);
+    // Read all rows from table
+    request = new Request(
+    'SELECT id, summa, fname, date, phone_number FROM cards.dbo.list WHERE id = '+id+';',
+    function(err, rowCount, rows) {
+    if (err) {
+        callback(err);
+    } else {
+        console.log(rowCount + ' row(s) returned');
+        if (rowCount == 0){
+          var result = {};
+          result.phone_number = "notRegistred";
+        }
+        else if (rowCount == 1){
+          var result = {};
+          var row = rows[0];
+          result.id = row[0].value;
+          result.summa = row[1].value;
+          result.fname = row[2].value;
+          result.date = row[3].value;
+          result.phone_number = row[4].value;
+
+
+
+
+
+
+      }
+      else if (rowCount > 1){
+        var result = {};
+        result.phone_number = "manyRegistred";
+      }
+      callback(null, result, res);
+    }
+    });
+
+    // Print the rows read
+
+    // Execute SQL statement
+    connection.execSql(request);
+}
+
+function sendPin(phone_number, summa, callback, res) {
+    var pin = getRandomInt(1000, 9999);
+    var id = phone_number;
+    // Read all rows from table
+
+    request2 = new Request(
+        'UPDATE cards.dbo.list SET pin=@pin WHERE phone_number = '+id+';',
+        function(err, rowCount, rows) {
+            if (err) {
+            callback(err, err, res);
+            } else {
+            console.log(rowCount + ' - Generating pin for phone '+id+', pin '+pin+' added');
+
+            request = new Request(
+            'SELECT pin FROM cards.dbo.list WHERE phone_number = '+phone_number+';',
+            function(err, rowCount, rows) {
+            if (err) {
+                callback(err);
+            } else {
+                console.log(rowCount + ' строчек найдено при проверке ПИН кода');
+                if (rowCount == 0){
+                  var result = {};
+                  result.phone_number = "notRegistred";
+                  callback(null, result, res);
+                }
+                else if (rowCount == 1){
+                  var result = {};
+                  var row = rows[0];
+                  result.pin = row[0].value;
+                  console.log('Sending SMS to ' + phone_number);
+            http.get('http://newbsms.tele2.ru/api/?operation=send&login=http_P2qZz1dJ&password=VpdJUgNe&msisdn=7'+phone_number+'&shortcode=RB24&text=Pincode: '+result.pin+' . Operatsiya spisanya '+summa+' bonusov. ', (ress) => {
+            const { statusCode } = ress;
+            const contentType = ress.headers['content-type'];
+            callback(null, result, res);
+            let error;
+            if (statusCode !== 200) {
+              error = new Error('Request Failed.\n' +
+                                `Status Code: ${statusCode}`);
+            } else if (!/^application\/json/.test(contentType)) {
+              error = new Error('Invalid content-type.\n' +
+                                `Expected application/json but received ${contentType}`);
+            }
+            if (error) {
+              console.error(error.message);
+              // consume response data to free up memory
+              ress.resume();
+              return;
+            }
+
+            ress.setEncoding('utf8');
+            let rawData = '';
+            ress.on('data', (chunk) => { rawData += chunk; });
+            ress.on('end', () => {
+              try {
+                const parsedData = JSON.parse(rawData);
+                console.log(parsedData);
+              } catch (e) {
+                console.error(e.message);
+              }
+            });
+
+          }).on('error', (e) => {
+            console.error(`Got error: ${e.message}`);
+          });
+
+
+              }
+              else if (rowCount > 1){
+                var result = {};
+                result.phone_number = "manyRegistred";
+                callback(null, result, res);
+              }
+
+
+            }
+            });
+
+            // Print the rows read
+
+            // Execute SQL statement
+            connection.execSql(request);
+
+
+
+            }
+        });
+        request2.addParameter('pin', TYPES.Decimal, pin);
+
+
+        // Execute SQL statement
+        connection.execSql(request2);
+
+
+
+
+
+}
+
+function register(id, fname, date, phone_number, callback, res) {
+    console.log("Попытка регистрации номера телефона: "+phone_number);
+
+          request = new Request(
+          'SELECT phone_number FROM cards.dbo.list WHERE phone_number = '+phone_number+';',
+          function(err, rowCount, rows) {
+          if (err) {
+              callback(err, "Ошибка SQL", res);
+          } else {
+              console.log("Колонок считано при провереке: "+rowCount);
+              if (rowCount == 0){
+
+                if (id > 100){
+                  request3 = new Request(
+                  'SELECT id, summa FROM cards.dbo.list WHERE id = '+id+';',
+                  function(err, rowCount, rows) {
+                  if (err) {
+                      callback(err, err, res);
+                  } else {
+                      var regSumma = 0;
+                      rows.forEach(function(row){
+                        regSumma += row[1].value
+                      });
+                      console.log(regSumma+ ' баллов будет перенесено на новую карту ' + id);
+                      request4 = new Request(
+                      'DELETE FROM cards.dbo.list WHERE id = '+id+';',
+                      function(err, rowCount, rows) {
+                      if (err) {
+                          callback(err, err, res);
+                      } else {
+
+                          console.log('Удалены старые записи карты: ' + id);
+
+                          request5 = new Request(
+                          'INSERT INTO cards.dbo.list (id, fname, phone, date, phone_number, summa) OUTPUT INSERTED.Id VALUES (@id, @fname, @phone, @date, @phone_number, @summa);',
+                          function(err, rowCount, rows) {
+                          if (err) {
+                              callback(err, err, res);
+                          } else {
+                              console.log(rowCount + ' записей добавлено');
+                              callback(null, 'Запись добавлена c сохранением баллов', res);
+                          }
+                          });
+
+                      request5.addParameter('id', TYPES.NChar, id);
+                      request5.addParameter('fname', TYPES.NChar, fname);
+                      request5.addParameter('phone', TYPES.Bit, 1);
+                      request5.addParameter('date', TYPES.Date, date);
+                      request5.addParameter('phone_number', TYPES.NChar, phone_number);
+                      request5.addParameter('summa', TYPES.Decimal, regSumma);
+                      // Execute SQL statement
+                      connection.execSql(request5);
+                      }
+                      });
+                      connection.execSql(request4);
+                  }
+                  });
+                  connection.execSql(request3);
+                }
+
+                else {
+
+
+                  console.log("Вложенный запрос:" +phone_number);
+                  request2 = new Request(
+                  'INSERT INTO cards.dbo.list (id, fname, phone, date, phone_number) OUTPUT INSERTED.Id VALUES (@id, @fname, @phone, @date, @phone_number);',
+                  function(err, rowCount, rows) {
+                  if (err) {
+                      callback(err, err, res);
+                  } else {
+                      console.log(rowCount + ' записей добавлено');
+                      callback(null, 'Запись добавлена', res);
+                  }
+                  });
+
+                  request2.addParameter('id', TYPES.NChar, id);
+                  request2.addParameter('fname', TYPES.NChar, fname);
+                  request2.addParameter('phone', TYPES.Bit, 1);
+                  request2.addParameter('date', TYPES.Date, date);
+                  request2.addParameter('phone_number', TYPES.NChar, phone_number);
+                  // Execute SQL statement
+                  connection.execSql(request2);
+
+
+                }
+
+            }
+            else {
+
+              callback(1, 'Запись с номером '+phone_number+' существует', res);
+            }
+          }
+          });
+
+          // Execute SQL statement
+          connection.execSql(request);
+
+
+
+
+
+
+}
+
+
+
+  connection.on('connect', function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Connected');
+
+      // Execute all functions in the array serially
+      //async.waterfall(array, Complete)
+    }
+  });
+
+  connection.on('end', function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('End');
+
+      // Execute all functions in the array serially
+      //async.waterfall(array, Complete)
+    }
+  });
+
+
+
+function edit( phone_number, action, summa, pincode, callback, res){
+
+  summa = parseInt(summa);
+  console.log("Изменение суммы бонусов по номеру телефона: "+phone_number);
+
+  request = new Request(
+  'SELECT phone_number, summa, pin FROM cards.dbo.list WHERE phone_number = '+phone_number+';',
+  function(err, rowCount, rows) {
+  if (err) {
+      callback(err, "Ошибка SQL", res);
+  } else {
+      console.log("Колонок считано при провереке: "+rowCount);
+
+      var result = {};
+      if (rowCount == 1){
+      var row = rows[0];
+      result.phone = phone_number;
+      result.summa = row[1].value;
+      var pin = row[2].value;
+      if (result.summa == null ){
+        result.summa = 0;
+      }
+      if (action == 'minus' ){
+        result.summa = result.summa - summa;
+        if (pincode == 55555){
+
+          pincode = 999;
+          pin = 999;
+        }
+      }
+      else if (action == 'plus'){
+        result.summa = result.summa + summa;
+        pincode = 999;
+        pin = 999;
+      }
+
+    }
+      if (rowCount == 1 && result.summa >= 0 && pincode == pin){
+
+
+        request = new Request(
+            'UPDATE cards.dbo.list SET summa=@summa WHERE phone_number = @phone_number;',
+            function(err, rowCount, rows) {
+                if (err) {
+                callback(err, err, res);
+                } else {
+                console.log(rowCount + ' row(s) updated');
+                callback(null, result, res);
+                }
+            });
+            request.addParameter('summa', TYPES.Decimal, result.summa);
+            request.addParameter('phone_number', TYPES.NVarChar, phone_number);
+
+            // Execute SQL statement
+            connection.execSql(request);
+
+    }
+    else {
+
+      if (pincode != pin){
+        callback(2, 'Пинкод не верный', res);
+      }
+      else
+      {
+        callback(1, 'Запись с номером '+phone_number+' не существует или их больше одной или попытка списать больше бонусов чем есть', res);
+      }
+
+    }
+  }
+  });
+
+  // Execute SQL statement
+  connection.execSql(request);
+
+}
+
+function editCard( id, action, summa, callback, res){
+  console.log('Reading Card Info...');
+  id = +id;
+ if (id > 999999999 ){
+    id = 01;
+  }
+  summa = parseInt(summa);
+
+  request = new Request(
+  'SELECT id, summa FROM cards.dbo.list WHERE id = '+id+';',
+  function(err, rowCount, rows) {
+  if (err) {
+      callback(err, err, res);
+  } else {
+      console.log("Колонок считано при провереке: "+rowCount);
+
+      var result = {};
+      if (rowCount == 1){
+      var row = rows[0];
+      result.id = id;
+      result.summa = row[1].value;
+      if (result.summa == null ){
+        result.summa = 0;
+      }
+      if (action == 'minusCard'){
+        result.summa = result.summa - summa;
+      }
+      else if (action == 'plusCard'){
+        result.summa = result.summa + summa;
+      }
+    }
+      if (rowCount == 1 && result.summa >= 0 ){
+
+
+        request = new Request(
+            'UPDATE cards.dbo.list SET summa=@summa WHERE id = '+id+';',
+            function(err, rowCount, rows) {
+                if (err) {
+                callback(err, err, res);
+                } else {
+                console.log(rowCount + ' row(s) updated');
+                callback(null, result, res);
+                }
+            });
+            request.addParameter('summa', TYPES.Decimal, result.summa);
+
+
+            // Execute SQL statement
+            connection.execSql(request);
+
+    }
+
+    else {
+
+      callback(1, 'Карта с номером '+id+' не существует или их больше одной или попытка списать больше бонусов чем есть', res);
+    }
+  }
+  });
+
+  // Execute SQL statement
+  connection.execSql(request);
+
+}
+
+
+exports.edit = edit;
+exports.register = register;
+exports.readInfo = readInfo;
+exports.readInfoCard = readInfoCard;
+exports.editCard = editCard;
+exports.sendPin = sendPin;
